a744a0bb98b10a2b6e6fcf380f6dbb99f5492e0d
Darkoo04 <baptiste.hesnard@proton.me>
2026-03-08 12:05:15 +0100

    Update base URL in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base:  '/openbento-darkoos_04.fr/',
+  base:  '/openbento/',
+  //base:  '/openbento-darkoos_04.fr/',
 })
